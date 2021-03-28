9e0abb11601358647fd67b0152f3fd9fa92f5efd
Ryan O'Donohue <rod.odonohue@gmail.com>
2021-03-28 12:25:45 -0400

    created for loop with inner each loop to display values from local storage.

--- script.js
+++ script.js
@@ -1,26 +1,35 @@
 var today = moment();
 $("#currentDay").text(today.format("MMM Do, YYYY"));
 
-var now = today.format('H')
 var hour = moment().hour();
-
 
 var saveBtns = $(".saveBtn");
 var inputs = $(".col-12")
 var tableRows = $(".tableRow");
 var valuesArr = [];
 
+
 saveBtns.each(function (index, ele) {
     ele.addEventListener("click", function () {
-        valuesArr.push(inputs[index].value);
-        localStorage.setItem("Key", valuesArr);
-        console.log(valuesArr)
+
+        var object = {
+            position: index,
+            value: inputs[index].value,   
+        };
+        
+        valuesArr.push(object);
+        localStorage.setItem("Key", JSON.stringify(valuesArr));   
     });
 });
+var savedInputs = JSON.parse(localStorage.getItem("Key")) || [];
 
-inputs.each(function (index, ele){
-    ele.value = localStorage.getItem("Key")
-})
+for (var i = 0; i <savedInputs.length; i++){
+    inputs.each(function (index, ele){
+        if (index === savedInputs[i].position){
+            ele.value = savedInputs[i].value;
+        }
+    });
+};
 
 tableRows.each(function (index, ele) {
     var calHour = parseInt(ele.id);
